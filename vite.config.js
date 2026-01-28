9f8ff03033d0643d482ff1415718792450512212
Heang <sothlyheang81@gmail.com>
2026-01-28 14:06:35 +0700

    phone update

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  appType: 'spa',
   server: {
     middlewareMode: false,
-    historyApiFallback: true,
   },
 })
